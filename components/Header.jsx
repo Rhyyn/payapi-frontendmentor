e505f8e9f79ae5f92499ce3ccc368a2357c50a0d
Tony Migeon <rhyyn.lol@gmail.com>
2022-06-21 17:37:20 +0200

    Now responsive

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import styles from '../styles/Header.module.scss'
-import NavBar from './NavBar'
+import React from "react";
+import styles from "../styles/Header.module.scss";
+import NavBar from "./NavBar";
+import Image from "next/image";
 
 const Header = () => {
-  return (
-    <header className={styles.headerContainer}>
-      <div className = {styles.mainNavContainer}>
-        <h1>payapi</h1>
-        <NavBar></NavBar>
-      </div>
-    </header>
-  )
-}
+    return (
+        <header className={styles.headerContainer}>
+            <div className={styles.mainNavContainer}>
+                <Image src="/logo.svg" width={135} height={38}></Image>
+                <NavBar></NavBar>
+            </div>
+            <button className = {styles.scheduleButton}>Schedule a Demo</button>
+        </header>
+    );
+};
 
-export default Header
+export default Header;
--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,14 +1,41 @@
-import React from 'react'
-import styles from '../styles/Navbar.module.scss'
+import React, { useState, useEffect } from "react";
+import styles from "../styles/Navbar.module.scss";
+import Image from "next/image";
 
 const NavBar = () => {
-  return (
-    <div className={styles.navBarContainer}>
-        <li>Pricing</li>
-        <li>About</li>
-        <li>Contact</li>
-    </div>
-  )
-}
+  const navItems = ["Pricing", "About", "Contact"]
+  const [windowSize, setWindowSize] = useState({
+    width: undefined,
+    height: undefined,
+  });
 
-export default NavBar
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      function handleResize() {
+        setWindowSize({
+          width: window.innerWidth,
+          height: window.innerHeight,
+        });
+      }
+      window.addEventListener("resize", handleResize);
+      handleResize();
+      return () => window.removeEventListener("resize", handleResize);
+    }
+  }, []);
+
+
+    // console.log(windowDimensions.width + " " + windowDimensions.height);
+    return (
+        <React.Fragment>
+            {windowSize.width >= 720 ? (
+                <div className={styles.navBarContainer}>
+                    {navItems.map((item, index) => {
+                        return <li key={index}>{item}</li>;
+                    })}
+                </div>
+            ) : <Image className={styles.menuIcon} src="/menu.svg" width={28} height={17}></Image>}
+        </React.Fragment>
+    );
+};
+
+export default NavBar;
