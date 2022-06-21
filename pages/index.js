e880431f6b23c40ada7198a790e6976fa46ea028
Tony Migeon <rhyyn.lol@gmail.com>
2022-06-21 12:28:53 +0200

    added Header

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.scss'
+import Header from '../components/Header'
 
 export default function Home() {
   return (
     <div>
+    <Header></Header>
       <h1 className={styles.test}>Hello</h1>
       <div className={styles.logo}>
         <Image src="/logo.svg" height={32} width={32}></Image>
