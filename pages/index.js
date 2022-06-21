828c04b06bedd0ce32ce56b35062cf533323f7ba
Tony Migeon <rhyyn.lol@gmail.com>
2022-06-21 17:37:39 +0200

    Removed useless things

--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,8 @@ import Header from '../components/Header'
 
 export default function Home() {
   return (
-    <div>
-    <Header></Header>
-      <h1 className={styles.test}>Hello</h1>
-      <div className={styles.logo}>
-        <Image src="/logo.svg" height={32} width={32}></Image>
-      </div>
+    <div className = {styles.main}>
+      <Header></Header>
     </div>
   )
 }
