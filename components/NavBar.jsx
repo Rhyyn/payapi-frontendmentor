615966e6d3733345ad8cea87462dfb2ec2b86c32
Tony Migeon <rhyyn.lol@gmail.com>
2022-06-22 13:00:22 +0200

    Opening and closing of NavMenu for mobile

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -3,28 +3,41 @@ import styles from "../styles/Navbar.module.scss";
 import Image from "next/image";
 
 const NavBar = () => {
-  const navItems = ["Pricing", "About", "Contact"]
-  const [windowSize, setWindowSize] = useState({
-    width: undefined,
-    height: undefined,
-  });
+    const navItems = ["Pricing", "About", "Contact"];
+    const [windowSize, setWindowSize] = useState({
+        width: undefined,
+        height: undefined,
+    });
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [closed, setIsClosed] = useState();
 
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      function handleResize() {
-        setWindowSize({
-          width: window.innerWidth,
-          height: window.innerHeight,
-        });
-      }
-      window.addEventListener("resize", handleResize);
-      handleResize();
-      return () => window.removeEventListener("resize", handleResize);
+    useEffect(() => {
+        if (typeof window !== "undefined") {
+            function handleResize() {
+                setWindowSize({
+                    width: window.innerWidth,
+                    height: window.innerHeight,
+                });
+            }
+            window.addEventListener("resize", handleResize);
+            handleResize();
+            return () => window.removeEventListener("resize", handleResize);
+        }
+    }, []);
+
+    const handleMenuOpen = () => {
+        setIsMenuOpen(true);
+    };
+
+    const handleClass = () => {
+      setIsClosed("");
+      setIsMenuOpen(false);
     }
-  }, []);
+    const handleMenuClose = () => {
+        setIsClosed(styles.menuBackgroundClose)
+        setTimeout(handleClass, 350)
+    };
 
-
-    // console.log(windowDimensions.width + " " + windowDimensions.height);
     return (
         <React.Fragment>
             {windowSize.width >= 720 ? (
@@ -33,7 +46,26 @@ const NavBar = () => {
                         return <li key={index}>{item}</li>;
                     })}
                 </div>
-            ) : <Image className={styles.menuIcon} src="/menu.svg" width={28} height={17}></Image>}
+            ) : null}
+            {!isMenuOpen && windowSize.width <= 720 ? (
+                <Image
+                    onClick={handleMenuOpen}
+                    className={styles.menuIcon}
+                    src="/menu.svg"
+                    width={28}
+                    height={17}
+                ></Image>
+            ) : null}
+            {isMenuOpen && windowSize.width <= 720 ? (
+                <Image
+                    onClick={handleMenuClose}
+                    className={styles.menuIcon}
+                    src="/close.svg"
+                    width={22}
+                    height={23}
+                ></Image>
+            ) : null}
+            {isMenuOpen && windowSize.width <= 720 ? <div className = {`${styles.menuBackground } ${closed}`}></div> : null}
         </React.Fragment>
     );
 };
