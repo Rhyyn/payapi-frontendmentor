204767b475ffc702a0c8f8809b2061362ac65133
Tony Migeon <rhyyn.lol@gmail.com>
2022-06-24 16:16:54 +0200

    Layou change, work on circle background pattern

--- pages/index.js
+++ pages/index.js
@@ -2,47 +2,100 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.scss";
 import Header from "../components/Header";
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export default function Home() {
+    const [windowSize, setWindowSize] = useState({
+        width: undefined,
+        height: undefined,
+    });
+    useEffect(() => {
+        if (typeof window !== "undefined") {
+            function handleResize() {
+                setWindowSize({
+                    width: window.innerWidth,
+                    height: window.innerHeight,
+                });
+            }
+            window.addEventListener("resize", handleResize);
+            handleResize();
+            return () => window.removeEventListener("resize", handleResize);
+        }
+    }, []);
     return (
         <React.Fragment>
-            <div className={styles.main}>
+            <section className={`${styles.main} ${styles.bgPattern}`}>
                 <Header></Header>
-                <div className={styles.mainContainer}>
-                    <div className={styles.sloganContainer}>
-                        <h1 className={styles.slogan}>
-                            Start building with our APIs for absolutely free.
-                        </h1>
-                        <div className={styles.emailInputContainer}>
-                            <form>
-                                <input
-                                    placeholder="Enter Email Adress"
-                                    className={styles.emailInput}
-                                    type="text"
-                                ></input>
-                            </form>
-                            <button>Schedule a Demo</button>
+                {windowSize.width >= 1100 ? ( // -------------DESKTOP
+                    <div className={styles.mainContainer}>
+                        <div className={styles.sloganContainer}>
+                            <h1 className={styles.slogan}>
+                                Start building with our APIs for absolutely
+                                free.
+                            </h1>
+                            <div className={styles.emailInputContainer}>
+                                <form>
+                                    <input
+                                        placeholder="Enter Email Adress"
+                                        className={styles.emailInput}
+                                        type="text"
+                                    ></input>
+                                </form>
+                                <button>Schedule a Demo</button>
+                            </div>
+                            <div className={styles.contactTextContainer}>
+                                <p>Have Any Questions?</p>
+                                <p>
+                                    <strong>Contact Us</strong>
+                                </p>
+                            </div>
                         </div>
-                        <div className={styles.contactTextContainer}>
-                            <p>Have Any Questions?</p>
-                            <p>
-                                <strong>Contact Us</strong>
-                            </p>
+                        <div className={styles.phoneContainer}>
+                            <img src="/illustration-phone-mockup.svg" />
                         </div>
                     </div>
-                    <div className = {styles.phoneContainer}>
-                        <img src="/illustration-phone-mockup.svg" />
+                ) : (
+                    // -------------TABLET
+                    <div className={styles.mainContainerMobile}>
+                        <div className={styles.phoneContainerMobile}>
+                            <img
+                                className={styles.phoneMockup}
+                                src="/illustration-phone-mockup.svg"
+                            />
+                        </div>
+                        <div className={styles.sloganContainerMobile}>
+                            <h1 className={styles.slogan}>
+                                Start building with our APIs for absolutely
+                                free.
+                            </h1>
+                            <div className={styles.emailInputContainer}>
+                                <form>
+                                    <input
+                                        placeholder="Enter Email Adress"
+                                        className={styles.emailInput}
+                                        type="text"
+                                    ></input>
+                                </form>
+                                <button>Schedule a Demo</button>
+                            </div>
+                            <div className={styles.contactTextContainer}>
+                                <p>Have Any Questions?</p>
+                                <p>
+                                    <strong>Contact Us</strong>
+                                </p>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-            <div className={styles.bgPattern}>
+                )}
+                <div className={styles.partnerContainer}></div>
+            </section>
+            {/* <div className={styles.bgPattern}>
                 <Image
                     src="/bg-pattern-circle.svg"
                     height={780}
                     width={780}
                 ></Image>
-            </div>
+            </div> */}
         </React.Fragment>
     );
 }
