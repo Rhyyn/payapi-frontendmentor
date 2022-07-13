4bea22b8d0c4b92442c00d10b0a4861090d1ba27
Tony Migeon <rhyyn.lol@gmail.com>
2022-07-13 16:27:17 +0200

    Implement Section

---
+++ components/Implement.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styles from "../styles/Implement.module.scss";
+
+const Implement = () => {
+    return (
+        <div className={styles.mainContainer}>
+            <img src="/illustration-easy-to-implement.svg" />
+            <div className = {styles.TextContainer}>
+                <h3 className={styles.Title}>Easy to implement</h3>
+                <p className = {styles.Text}>
+                    Our API comes with just a few lines of code. You’ll be up
+                    and running in no time. We built our documentation page to
+                    integrate payments functionality with ease.
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default Implement;
