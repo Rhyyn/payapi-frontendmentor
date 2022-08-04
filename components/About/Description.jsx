983456df1090df6d22b5ae48d1742ea12d516503
Tony Migeon <rhyyn.lol@gmail.com>
2022-08-04 17:27:20 +0200

    Start of Basic Styling

--- components/About/Description.jsx
+++ components/About/Description.jsx
@@ -4,7 +4,7 @@ import styles from "../../styles/About/Description.module.scss";
 const Description = () => {
     return (
         <div className = {styles.mainContainer}>
-            <h2 className = {styles.title}>We Empower innovators by delivering access to the financial system</h2>
+            <h2 className = {styles.title}>We empower innovators by delivering access to the financial system</h2>
             <h3 className = {styles.paragraphTitle}>Our Vision</h3>
             <p className = {styles.paragraph}>Our main goal is to build beautiful consumer experiences along with developer-friendly infrastructure. The result is an intelligent tool that gives everyone the ability to create amazing products that solve big problems. We are deeply focused on democratizing financial services through technology. </p>
             <h3 className = {styles.paragraphTitle}>Our Business</h3>
