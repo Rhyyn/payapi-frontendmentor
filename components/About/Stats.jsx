97f75d5751064a9622340529e6c11245b1d24099
Tony Migeon <rhyyn.lol@gmail.com>
2022-08-05 16:50:42 +0200

    Mobile Layout

--- components/About/Stats.jsx
+++ components/About/Stats.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import styles from "../../styles/About/Stats.module.scss"
+import React from "react";
+import styles from "../../styles/About/Stats.module.scss";
 
 const Stats = () => {
-  return (
-    <div>Stats</div>
-  )
-}
+    return (
+        <div className={styles.mainContainer + " " + styles.borderTop}>
+            <div className={styles.individualContainer + " " + styles.paddingTop}>
+                <h4 className={styles.statTitle}>Team Members</h4>
+                <h1 className={styles.number}>300+</h1>
+            </div>
+            <div className={styles.individualContainer}>
+                <h4 className={styles.statTitle}>Offices in the US</h4>
+                <h1 className={styles.number}>3</h1>
+            </div>
+            <div
+                className={
+                    styles.individualContainer + " " + styles.borderBottom}>
+                <h4 className={styles.statTitle}>Transactions analyzed</h4>
+                <h1 className={styles.number}>10M+</h1>
+            </div>
+        </div>
+    );
+};
 
-export default Stats
+export default Stats;
