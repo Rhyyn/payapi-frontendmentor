3c7f76b7d06c3044e5cf9e00722aecd83a50f690
Tony Migeon <rhyyn.lol@gmail.com>
2022-08-06 13:02:44 +0200

    Mobile Layout

--- components/About/Environment.jsx
+++ components/About/Environment.jsx
@@ -3,7 +3,13 @@ import styles from "../../styles/About/Environment.module.scss"
 
 const Environment = () => {
   return (
-    <div>Environment</div>
+    <div className = {styles.mainContainer}>
+            <h3 className = {styles.paragraphTitle}>The Culture</h3>
+            <p className = {styles.paragraph}>We strongly believe there's always an opportunity to learn from each other outside of day-to-day work, whether it's company-wide offsites, internal hackathons, or co-worker meetups. We always value cross-team collaboration and diversity of thought, no matter the job title.</p>
+            <h3 className = {styles.paragraphTitle}>The People</h3>
+            <p className = {styles.paragraph}> We're all passionate about building a more efficient and inclusive financial infrastructure together. At PayAPI, we have diverse backgrounds and skills.</p>
+            <div className = {styles.image}></div>
+        </div>
   )
 }
 
