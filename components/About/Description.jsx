bfba195c8e769157e65070040f5dde6cbb8c8292
Tony Migeon <rhyyn.lol@gmail.com>
2022-08-09 15:54:32 +0200

    Tablet + desktop layout

--- components/About/Description.jsx
+++ components/About/Description.jsx
@@ -4,11 +4,18 @@ import styles from "../../styles/About/Description.module.scss";
 const Description = () => {
     return (
         <div className={styles.mainContainer}>
-            <h2 className={styles.title}>We empower </h2>
-            <h2 className={styles.title}>innovators</h2>
-            <h2 className={styles.title}>by delivering access to</h2>
-            <h2 className={styles.title}>the financial system</h2>
-            <div className = {styles.paragraphContainer}>
+            <h2 className={styles.mobileTitle}>We empower </h2>
+            <h2 className={styles.mobileTitle}>innovators</h2>
+            <h2 className={styles.mobileTitle}>by delivering access to</h2>
+            <h2 className={styles.mobileTitle}>the financial system</h2>
+            <div className = {styles.tabletTitleContainer}>
+                <h2 className={styles.tabletTitle}>We empower innovators</h2>
+                <h2 className={styles.tabletTitle}>
+                    by delivering access to the
+                </h2>
+                <h2 className={styles.tabletTitle}>financial system</h2>
+            </div>
+            <div className={styles.paragraphContainer}>
                 <h3 className={styles.paragraphTitle}>Our Vision</h3>
                 <p className={styles.paragraph}>
                     Our main goal is to build beautiful consumer experiences
@@ -19,7 +26,7 @@ const Description = () => {
                     technology.
                 </p>
             </div>
-            <div className = {styles.paragraphContainer}>
+            <div className={styles.paragraphContainer}>
                 <h3 className={styles.paragraphTitle}>Our Business</h3>
                 <p className={styles.paragraph}>
                     At the core of our platform is the technical infrastructure
