f19ff49a8a003d509e98a14d41a0bd6228fc3fa2
Tony Migeon <rhyyn.lol@gmail.com>
2022-08-10 16:23:40 +0200

    Start of mobile contact Form

--- components/Contact/Form.jsx
+++ components/Contact/Form.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
+import styles from "../../styles/Contact/Form.module.scss";
 
 const Form = () => {
-  return (
-    <div>Form</div>
-  )
-}
+    return (
+        <div className={styles.mainContainer}>
+            <form className={styles.form}>
+                <input
+                    className={styles.input}
+                    type="text"
+                    placeholder="Name"
+                ></input>
+                <input
+                    className={styles.input}
+                    type="text"
+                    placeholder="Email Adress"
+                ></input>
+                <input
+                    className={styles.input}
+                    type="text"
+                    placeholder="Company Name"
+                ></input>
+                <input
+                    className={styles.input}
+                    type="text"
+                    placeholder="Title"
+                ></input>
+                <input
+                    className={styles.input + " " + styles.inputMessage}
+                    type="text"
+                    placeholder="Message"
+                ></input>
+                <div className = {styles.checkboxContainer}>
+                    <input className = {styles.checkbox} type="checkbox"></input>
+                    <p className = {styles.checkboxDescription}>
+                        Stay up-to-date with company announcements and updates
+                        to our API
+                    </p>
+                </div>
+            </form>
+        </div>
+    );
+};
 
-export default Form
+export default Form;
